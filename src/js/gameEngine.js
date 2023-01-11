297b17ebe34f323a1df7cb560ee199bec4d382b4
Denitsa Slavkova <deni9309@gmail.com>
2023-01-11 22:03:36 +0200

    Add bug element with random delay on create

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -4,25 +4,21 @@ function start(state, game) {
 
     // bind 'state' and 'game' to the function's context
     // therefore we can pass them through when function executes
-    window.requestAnimationFrame(gameLoop.bind(null, state, game));
+    window.requestAnimationFrame((timeStamp) => gameLoop(state, game, timeStamp));
 }
 
-function gameLoop(state, game) {
+function gameLoop(state, game, timeStamp) {
     const { wizard } = state;
     const { wizardElement } = game;
 
-    // move wizard D:right W:up A:left S:down
-    if (state.keys.KeyD) {
-        wizard.posX += wizard.speed;
-    }
-    if (state.keys.KeyW) {
-        wizard.posY -= wizard.speed;
-    }
-    if (state.keys.KeyA) {
-        wizard.posX -= wizard.speed;
-    }
-    if (state.keys.KeyS) {
-        wizard.posY += wizard.speed;
+    modifyWizardPosition(state, game, timeStamp);
+
+    // spawn bugs 
+    if (timeStamp > state.bugStats.nextSpawnTimestamp) {
+        game.createBug(state.bugStats);
+        
+        // configure when to show new bug with timestamp
+        state.bugStats.nextSpawnTimestamp = timeStamp + Math.random() * state.bugStats.maxSpawnInterval;
     }
 
     // render
@@ -30,4 +26,24 @@ function gameLoop(state, game) {
     wizardElement.style.top = wizard.posY + 'px';
 
     window.requestAnimationFrame(gameLoop.bind(null, state, game));
+}
+
+function modifyWizardPosition(state, game) {
+    const { wizard } = state;
+
+    // move wizard D:right W:up A:left S:down
+    if (state.keys.KeyD) {
+        // to be fully visible when moving right (stays within the screen dimensions )
+        wizard.posX = Math.min(wizard.posX + wizard.speed, game.gameScreen.offsetWidth - wizard.width);
+    }
+    if (state.keys.KeyW) {
+        wizard.posY = Math.max(wizard.posY - wizard.speed, 0);
+    }
+    if (state.keys.KeyA) {
+        wizard.posX = Math.max(wizard.posX - wizard.speed, 0);
+    }
+    if (state.keys.KeyS) {
+        // to be fully visible when moving down (stays within the screen dimensions )
+        wizard.posY = Math.min(wizard.posY + wizard.speed, game.gameScreen.offsetHeight - wizard.height);
+    }
 }
--- src/js/gameState.js
+++ src/js/gameState.js
@@ -5,14 +5,23 @@ function initState() {
     const state = {
         player: 'Merlin',
         wizard: {
-            width: 82,
+            width: 87,
             height: 100,
             posX: startX,
             posY: startY,
             speed: 4
         },
+        bugStats: {
+            width:50,
+            height: 50,
+            nextSpawnTimestamp: 0,
+            maxSpawnInterval: 1500,
+        },
         keys: {
-
+            KeyA: false,
+            KeyD: false,
+            KeyW: false,
+            KeyS: false
         }
     }
 
